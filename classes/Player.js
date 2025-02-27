1e266fb9a6288fca182fe2341dc022f295748ae1
davidalantodd <107488171+davidalantodd@users.noreply.github.com>
2025-02-27 12:08:06 -0500

    add Item class and Player functionality with testing for both

--- classes/Player.js
+++ classes/Player.js
@@ -6,6 +6,18 @@ class Player extends Person {
         super(name, type, location)
         // set subclass property
         this.weapon = weapon
+        // initialize our empty inventory
+        this.inventory = []
+    }
+
+    addItem(item){
+        // check to be sure that the item doesn't already exist
+        if (!this.inventory.includes(item)) {
+            // add the item
+            this.inventory.push(item);
+            return;
+        }
+        console.log(`${item.name} already in inventory.`)
     }
 }
 
